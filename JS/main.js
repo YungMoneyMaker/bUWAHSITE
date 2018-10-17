3f8b02547c20238c564032aec0371f304c82930f
YungMoneyMaker <43042795+YungMoneyMaker@users.noreply.github.com>
2018-10-17 13:14:40 -0400

    sdfrgthjkl

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
 function test1(){
-	var fName = prompt("I'm gonna take them toes if I don't get that name bruh!")
-	var myColour = prompt("Pick a colour before u get them toes taking boi!" + fName)
+	var fName = prompt("I'm gonna take them toes if I don't get that name bruh!");
+	var myColour = prompt("Pick a colour before u get them toes taking boi!" + fName);
 }
