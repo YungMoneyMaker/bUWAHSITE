530f8252462ce153b01149c2d8e57ac9152b9f22
YungMoneyMaker <43042795+YungMoneyMaker@users.noreply.github.com>
2018-11-05 12:08:41 -0500

    f

--- JS/main.js
+++
@@ -1,4 +0,0 @@
-function test1(){
-	var fName = prompt("I'm gonna take them toes if I don't get that name bruh!");
-	var myColour = prompt("Pick a colour before u get them toes taking boi!" + fName);
-}
