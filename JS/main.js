a8e29d9b070893bbbd9268bbfe6ab63fdee26d07
YungMoneyMaker <43042795+YungMoneyMaker@users.noreply.github.com>
2018-11-07 13:13:02 -0500

    j

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+//Javascript Name and Colour Game// 
+function A1(){
+	var fName = prompt("Wagawn fham saying you have a nameazz??");
+	var myColour = promt("Yo pick a colour one time," +fName);
+
+	if (myColour == "red")
+}
