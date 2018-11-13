dd6bc83e620264d79ba79958be2ed9b0c805ae6f
bUWAH <yungbuwah@gmail.com>
2018-11-12 23:39:00 -0500

    a

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,31 @@ function A1(){
 
 	if (myColour == "red")
 }
+//Assignment 4 
+function assignment A4() {
+	for (let i = 1; i <= 100 i++){
+		if (i%3 === 0 && i%5 === 0) {
+			console.log("FizzBuzz")
+		}
+		else if (i%3 === 0) {
+			console.log("Fizz")
+		}
+		else if (if%3 === 0) {
+			console.log("Buzz")
+		}
+		else {
+			console.log(i)
+		}
+	}
+}
+//Triangle made out of astrics 
+function assignment B4 () {
+	var numZ = prompt ("How many intervals do you want in the triangle?")
+	var stars = " *"
+	numZ -= 1;
+	console.log("'"+stars+" '")
+	for (let 1 = 0; i <numZ i++){
+		stars += " *"
+		console.log("'"+stars+" '")
+	}
+}
