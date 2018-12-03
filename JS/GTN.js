e72dd92c664771ef72037c960951247af6a33a16
YungMoneyMaker <43042795+YungMoneyMaker@users.noreply.github.com>
2018-12-03 12:47:28 -0500

    a

--- JS/GTN.js
+++ JS/GTN.js
@@ -1,5 +1,32 @@
 function GTN() {
+	
 	var x = Math.floor((Math.random()* 1000) +1);
-	//document.getElementById("RNG Program").innerHTML = x;
-	console.log (x); 
+	var attemps = 0
+
+	do {
+		var answer = prompt ("Guess any number between 1 and 1000, To exit this game type 'q'.");
+		var answer = document.getElementID("GTN")
+		++ attemps;
+		if (answer > 1000) {
+			alert("Invalid input, please answer with a number between 1 and 1000!");
+	{
+		else if (guess != int)
+			alert("Please enter an integer value!");
+	}
+	}
+		else if (guess < x)
+			alert("The answer is higher that that!");
+	}
+	{
+		else if (guess > x)
+			alert("The answer is lower!");
+	}
+	{
+		else if (guess == x)
+			alert("Correct, the right answer was " +rand +"!");
+			alert("You guessed the correst number in only " +attemps +" attemps!");
+	}
+	{
+		if (guess == "q") break;
+	}
 }
